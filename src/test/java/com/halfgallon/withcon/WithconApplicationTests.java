9b26b6f5b29543400a7aef49454f9af067e4871c
sosa7753 <sosa7753@naver.com>
2024-11-27 22:26:40 +0900

    [refactor] Load 테스트시 Filter와 통합 테스트를 위한 어노테이션 추가

--- src/test/java/com/halfgallon/withcon/WithconApplicationTests.java
+++ src/test/java/com/halfgallon/withcon/WithconApplicationTests.java
@@ -2,7 +2,11 @@ package com.halfgallon.withcon;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.test.context.web.WebAppConfiguration;
 
+@ServletComponentScan
+@WebAppConfiguration
 @SpringBootTest
 class WithconApplicationTests {
 
