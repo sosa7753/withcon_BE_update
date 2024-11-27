aed0fc47edccc479a34c474e3258d942dd26cd2a
sosa7753 <sosa7753@naver.com>
2024-11-27 22:23:56 +0900

    [refactor] Redis, JPA Repository 사용시 경고 처리 - Redis가 사용하는 Repository 명시 - JPA 스캔시 CrudRepository 스캔 제외

--- src/main/java/com/halfgallon/withcon/WithconApplication.java
+++ src/main/java/com/halfgallon/withcon/WithconApplication.java
@@ -1,13 +1,21 @@
 package com.halfgallon.withcon;
 
+import com.halfgallon.withcon.domain.auth.repository.AccessTokenRepository;
+import com.halfgallon.withcon.domain.auth.repository.RefreshTokenRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
+@EnableRedisRepositories(basePackages = {"com.halfgallon.withcon.domain.auth.repository"})
+@EnableJpaRepositories(excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = {
+		AccessTokenRepository.class, RefreshTokenRepository.class}))
 @SpringBootApplication
 public class WithconApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WithconApplication.class, args);
 	}
-
 }
