e93a474869d6238d50c00d4f65a7d5de8f94559c
sosa7753 <sosa7753@naver.com>
2024-11-27 22:33:01 +0900

    [refactor] Redis test DB를 활용하기 위한한 Bean 등록 변경
    - database index 1을 사용하기 위해 Bean 등록시 database index를 설정 정보에서 받아 Lettuce에 등록한다.

--- src/main/java/com/halfgallon/withcon/global/config/RedisConfig.java
+++ src/main/java/com/halfgallon/withcon/global/config/RedisConfig.java
@@ -4,15 +4,14 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.RedisMessageListenerContainer;
-import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
-@EnableRedisRepositories
 public class RedisConfig {
 
   @Value("${spring.data.redis.host}")
@@ -21,9 +20,16 @@ public class RedisConfig {
   @Value("${spring.data.redis.port}")
   private int port;
 
+  @Value("${spring.data.redis.database}")
+  private int index;
+
   @Bean
   public RedisConnectionFactory redisConnectionFactory() {
-    return new LettuceConnectionFactory(host, port);
+    RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+    redisStandaloneConfiguration.setHostName(host);
+    redisStandaloneConfiguration.setPort(port);
+    redisStandaloneConfiguration.setDatabase(index);
+    return new LettuceConnectionFactory(redisStandaloneConfiguration);
   }
 
   @Bean
