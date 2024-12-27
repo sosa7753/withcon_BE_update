a8b16f17cfa2605ef3333ad26dafdfdf993d2e29
sosa7753 <sosa7753@naver.com>
2024-12-27 13:42:44 +0900

    [001-NOTIFICATION-DIVISION][refactor] 채팅방 메시지 알림 로직 분할

---
+++ src/main/java/com/halfgallon/withcon/domain/notification/controller/event/ChatRoomEventController.java
@@ -0,0 +1,26 @@
+package com.halfgallon.withcon.domain.notification.controller.event;
+
+import com.halfgallon.withcon.domain.notification.service.handler.GenerateEvent;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/notification")
+@RequiredArgsConstructor
+public class ChatRoomEventController {
+
+  private final GenerateEvent event;
+
+  // 채팅방 메시지 발생 event
+  @PostMapping("/notification/event")
+  public ResponseEntity<Void> generateEvent(
+      @RequestParam Long chatRoomId) {
+
+    event.doSomething(chatRoomId);
+    return ResponseEntity.ok().build();
+  }
+}
