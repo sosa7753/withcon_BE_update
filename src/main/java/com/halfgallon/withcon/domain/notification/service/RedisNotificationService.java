c3752492a1fc57af117e10cd83e02e87cb69a827
sosa7753 <sosa7753@naver.com>
2024-12-27 13:44:06 +0900

    [001-NOTIFICATION-DIVISION][refactor] 클래스 분할에 변경사항 클래스

--- src/main/java/com/halfgallon/withcon/domain/notification/service/RedisNotificationService.java
+++ src/main/java/com/halfgallon/withcon/domain/notification/service/RedisNotificationService.java
@@ -1,7 +1,6 @@
 package com.halfgallon.withcon.domain.notification.service;
 
 import com.halfgallon.withcon.domain.notification.dto.NotificationResponse;
-import com.halfgallon.withcon.domain.notification.dto.VisibleRequest;
 
 public interface RedisNotificationService {
   void subscribe(String channel); // 채널 구독
@@ -9,7 +8,4 @@ public interface RedisNotificationService {
   void unsubscribe(String channel); // 구독 해지
 
   void publish(String channel, NotificationResponse notificationResponse); // 메세지 발행
-
-  void createVisibleCache(Long memberId, VisibleRequest request);
-
 }
--- src/main/java/com/halfgallon/withcon/domain/notification/service/impl/RedisNotificationServiceImpl.java
+++ src/main/java/com/halfgallon/withcon/domain/notification/service/impl/RedisNotificationServiceImpl.java
@@ -23,7 +23,6 @@ public class RedisNotificationServiceImpl implements RedisNotificationService {
   private final RedisMessageListenerContainer container;
   private final RedisListener subscriber;
   private final RedisTemplate<String, Object> redisTemplate;
-  private final RedisService redisService;
 
   @Override
   public void subscribe(String channel) {
@@ -44,27 +43,6 @@ public class RedisNotificationServiceImpl implements RedisNotificationService {
     log.info("redis 메세지 발행 성공");
   }
 
-  // Redis에 visible 데이터 저장.
-  @Override
-  public void createVisibleCache(Long memberId, VisibleRequest request) {
-    String hashKey = RedisCacheType.VISIBLE_CACHE.getDescription() + request.getChatRoomId();
-    log.info("VisibleCache 채널 명 : " + hashKey);
-
-    Map<Object, Object> visibleCaches = redisService.getHashByKey(hashKey);
-    log.info("Service : 특정 채팅방에 대한 Visible Map 조회 성공" + visibleCaches);
-
-    if (visibleCaches != null) { // 특정 채팅방의 Map이 이미 존재한다면
-      redisService.updateToHash(hashKey, String.valueOf(memberId), request.getVisibleType());
-      log.info("Redis Visible 기존 Map 변경 " + memberId +
-          " : " + request.getVisibleType());
-    } else {
-      Map<Object, Object> newObject = new HashMap<>();
-      newObject.put(String.valueOf(memberId), request.getVisibleType());
-      redisService.saveToHash(hashKey, newObject, 24);
-      log.info("Redis Visible 새로운 Map 추가 " + memberId +
-          " : " + request.getVisibleType());
-    }
-  }
 }
 
 
