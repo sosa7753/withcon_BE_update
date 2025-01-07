208ec613edabade93db5f053407bdbbe7d51956c
sosa7753 <141195262+sosa7753@users.noreply.github.com>
2025-01-08 00:09:43 +0900

    [004-KAFKA-SERIALIZER][feat] 직렬화 및 역직렬화 커스터마이징 (#5)
    * [004-KAFKA-CONFIG][feat] 직렬화 및 역직렬화 커스터마이징
    
    * [004-KAFKA-CONFIG][feat] 직렬화 및 역직렬화 커스터마이징

--- src/main/java/com/halfgallon/withcon/domain/notification/kafka/config/ProducerKafkaConfig.java
+++ src/main/java/com/halfgallon/withcon/domain/notification/kafka/config/ProducerKafkaConfig.java
@@ -1,5 +1,6 @@
 package com.halfgallon.withcon.domain.notification.kafka.config;
 
+import com.halfgallon.withcon.domain.notification.kafka.serializer.CustomJsonSerializer;
 import java.util.HashMap;
 import java.util.Map;
 import org.apache.kafka.clients.producer.ProducerConfig;
@@ -9,7 +10,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
-import org.springframework.kafka.support.serializer.JsonSerializer;
 
 @Configuration
 public class ProducerKafkaConfig {
@@ -19,8 +19,7 @@ public class ProducerKafkaConfig {
     Map<String, Object> config = new HashMap<>();
     config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
     config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-    config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
-
+    config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, CustomJsonSerializer.class);
     return new DefaultKafkaProducerFactory<>(config);
   }
 
