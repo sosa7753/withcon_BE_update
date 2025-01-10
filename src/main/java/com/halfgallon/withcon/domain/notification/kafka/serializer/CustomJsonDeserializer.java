97b5810437e097afe995e8ccf952ba373144b8e4
sosa7753 <sosa7753@naver.com>
2025-01-10 15:46:55 +0900

    [004-KAFKA-SERIALIZER][refactor] 직렬화 및 역직렬화 처리 변경

--- src/main/java/com/halfgallon/withcon/domain/notification/kafka/serializer/CustomJsonDeserializer.java
+++ src/main/java/com/halfgallon/withcon/domain/notification/kafka/serializer/CustomJsonDeserializer.java
@@ -1,16 +1,20 @@
 package com.halfgallon.withcon.domain.notification.kafka.serializer;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.halfgallon.withcon.domain.notification.kafka.dto.ChatRoomNotificationKafkaRequest;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.support.JacksonUtils;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
 
 public class CustomJsonDeserializer extends JsonDeserializer<ChatRoomNotificationKafkaRequest> {
 
-  public CustomJsonDeserializer() {}
+  public CustomJsonDeserializer() {
+    super(customObjectMapper());
+  }
 
-  @Autowired
-  public CustomJsonDeserializer(ObjectMapper objectMapper) {
-    super(objectMapper);
+  private static ObjectMapper customObjectMapper() {
+    ObjectMapper mapper = JacksonUtils.enhancedObjectMapper();
+    mapper.disable(SerializationFeature.WRITE_DATE_KEYS_AS_TIMESTAMPS);
+    return mapper;
   }
 }
--- src/main/java/com/halfgallon/withcon/domain/notification/kafka/serializer/CustomJsonSerializer.java
+++ src/main/java/com/halfgallon/withcon/domain/notification/kafka/serializer/CustomJsonSerializer.java
@@ -1,12 +1,20 @@
 package com.halfgallon.withcon.domain.notification.kafka.serializer;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.halfgallon.withcon.domain.notification.kafka.dto.ChatRoomNotificationKafkaRequest;
+import org.springframework.kafka.support.JacksonUtils;
 import org.springframework.kafka.support.serializer.JsonSerializer;
 
 public class CustomJsonSerializer extends JsonSerializer<ChatRoomNotificationKafkaRequest> {
 
-  public CustomJsonSerializer(ObjectMapper objectMapper) {
-    super(objectMapper);
+  public CustomJsonSerializer() {
+    super(customObjectMapper());
+  }
+
+  private static ObjectMapper customObjectMapper() {
+    ObjectMapper mapper = JacksonUtils.enhancedObjectMapper();
+    mapper.disable(SerializationFeature.WRITE_DATE_KEYS_AS_TIMESTAMPS);
+    return mapper;
   }
 }
