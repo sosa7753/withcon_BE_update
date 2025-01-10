fcd69324b1776532b10bb4ba9b36736be318d96a
sosa7753 <141195262+sosa7753@users.noreply.github.com>
2025-01-10 15:51:45 +0900

    [004-KAFKA-SERIALIZER][refactor] 직렬화 및 역직렬화 처리 변경 (#8)

--- src/main/java/com/halfgallon/withcon/domain/notification/kafka/config/SerializerConfig.java
+++ src/main/java/com/halfgallon/withcon/domain/notification/kafka/config/SerializerConfig.java
@@ -1,8 +1,5 @@
 package com.halfgallon.withcon.domain.notification.kafka.config;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.halfgallon.withcon.domain.notification.kafka.serializer.CustomJsonDeserializer;
 import com.halfgallon.withcon.domain.notification.kafka.serializer.CustomJsonSerializer;
 import org.springframework.context.annotation.Bean;
@@ -13,19 +10,11 @@ public class SerializerConfig {
 
   @Bean
   public CustomJsonSerializer customJsonSerializer() {
-    return new CustomJsonSerializer(customObjectMapper());
+    return new CustomJsonSerializer();
   }
 
   @Bean
   public CustomJsonDeserializer customJsonDeserializer() {
-    return new CustomJsonDeserializer(customObjectMapper());
-  }
-
-  @Bean
-  public ObjectMapper customObjectMapper() {
-    ObjectMapper objectMapper = new ObjectMapper();
-    objectMapper.registerModule(new JavaTimeModule());
-    objectMapper.disable(SerializationFeature.WRITE_DATE_KEYS_AS_TIMESTAMPS);
-    return objectMapper;
+    return new CustomJsonDeserializer();
   }
 }
