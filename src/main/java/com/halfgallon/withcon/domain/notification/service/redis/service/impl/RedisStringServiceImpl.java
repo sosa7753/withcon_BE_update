9aa1c0676814f74a66e753dfee98a182697ace56
sosa7753 <sosa7753@naver.com>
2025-01-13 16:00:21 +0900

    [010-REDIS-STRING-SERVICE][refactor] redis String 저장 및 삭제 실패시 예외 처리

--- src/main/java/com/halfgallon/withcon/domain/notification/service/redis/service/impl/RedisStringServiceImpl.java
+++ src/main/java/com/halfgallon/withcon/domain/notification/service/redis/service/impl/RedisStringServiceImpl.java
@@ -1,10 +1,15 @@
 package com.halfgallon.withcon.domain.notification.service.redis.service.impl;
 
+import static com.halfgallon.withcon.global.exception.ErrorCode.REDIS_SUBSCRIBE_FAIL;
+
 import com.halfgallon.withcon.domain.notification.service.redis.service.RedisStringService;
+import com.halfgallon.withcon.global.exception.CustomException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class RedisStringServiceImpl implements RedisStringService {
@@ -17,12 +22,20 @@ public class RedisStringServiceImpl implements RedisStringService {
   }
 
   @Override
-  public void save(String Key, String value) {
-    redisTemplate.opsForValue().set(Key, value);
+  public void save(String key, String value) {
+    try {
+      redisTemplate.opsForValue().set(key, value);
+    } catch (Exception e) {
+      throw new CustomException(REDIS_SUBSCRIBE_FAIL);
+    }
   }
 
   @Override
-  public void delete(String Key) {
-    redisTemplate.delete(Key);
+  public void delete(String key) {
+    try {
+      redisTemplate.delete(key);
+    } catch (Exception e) {
+      log.info("redis 삭제 실패 {}", key);
+    }
   }
 }
