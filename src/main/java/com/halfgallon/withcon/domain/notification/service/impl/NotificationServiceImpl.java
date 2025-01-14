f09ebea42aee1e77598a75eed1ae714b6809ec48
sosa7753 <sosa7753@naver.com>
2025-01-14 16:03:08 +0900

    [011-REDIS-SHARED-MEMORY][feat] Redis로 userId-서버Ip 데이터 처리

--- src/main/java/com/halfgallon/withcon/domain/notification/service/impl/NotificationServiceImpl.java
+++ src/main/java/com/halfgallon/withcon/domain/notification/service/impl/NotificationServiceImpl.java
@@ -9,11 +9,13 @@ import com.halfgallon.withcon.domain.notification.repository.NotificationReposit
 import com.halfgallon.withcon.domain.notification.repository.SseEmitterRepository;
 import com.halfgallon.withcon.domain.notification.service.NotificationService;
 import com.halfgallon.withcon.domain.notification.service.SseEmitterService;
+import com.halfgallon.withcon.domain.notification.service.redis.service.RedisStringService;
 import com.halfgallon.withcon.global.exception.CustomException;
 import java.util.List;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
@@ -29,9 +31,15 @@ public class NotificationServiceImpl implements NotificationService {
   private final SseEmitterRepository sseEmitterRepository;
   private final SseEmitterService sseEmitterService;
 
+  private final RedisStringService redisStringService;
+
+  @Value("${server.port}")
+  private int port;
 
   @Override
   public SseEmitter subscribe(Long memberId) {
+    redisStringService.save(String.valueOf(memberId), "localhost:" + port);
+
     String emitterId = createEmitterId(memberId);
     SseEmitter sseEmitter = sseEmitterRepository.save(emitterId, new SseEmitter(TIME_OUT));
 
@@ -43,6 +51,7 @@ public class NotificationServiceImpl implements NotificationService {
     sseEmitter.onCompletion(() -> {
       log.info("onCompletion callback");
       sseEmitterRepository.deleteById(emitterId);
+      redisStringService.delete(String.valueOf(memberId));
     });
     sseEmitter.onError((e) -> {
       log.info("onError callback");
@@ -51,6 +60,7 @@ public class NotificationServiceImpl implements NotificationService {
     sseEmitter.onTimeout(() -> {
       log.info("onTimeout callback");
       sseEmitterRepository.deleteById(emitterId);
+      redisStringService.delete(String.valueOf(memberId));
     });
 
     return sseEmitter;
--- src/main/java/com/halfgallon/withcon/domain/notification/service/redis/service/impl/RedisStringServiceImpl.java
+++ src/main/java/com/halfgallon/withcon/domain/notification/service/redis/service/impl/RedisStringServiceImpl.java
@@ -1,15 +1,22 @@
 package com.halfgallon.withcon.domain.notification.service.redis.service.impl;
 
 import com.halfgallon.withcon.domain.notification.service.redis.service.RedisStringService;
+import java.time.Duration;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class RedisStringServiceImpl implements RedisStringService {
 
   private final RedisTemplate<String, String> redisTemplate;
+
+  @Value("${redis_shared_memory_ttl}")
+  private Duration ttl;
 
   @Override
   public String get(String key) {
@@ -17,12 +24,16 @@ public class RedisStringServiceImpl implements RedisStringService {
   }
 
   @Override
-  public void save(String Key, String value) {
-    redisTemplate.opsForValue().set(Key, value);
+  public void save(String key, String value) {
+    redisTemplate.opsForValue().set(key, value);
+    log.info("공유 메모리 key : {}, value : {}", key, value);
+    redisTemplate.expire(key, ttl);
+    log.info("공유 메모리 key : {}, ttl : {}", key, ttl);
   }
 
   @Override
-  public void delete(String Key) {
-    redisTemplate.delete(Key);
+  public void delete(String key) {
+    redisTemplate.delete(key);
+    log.info("공유 메모리 key 삭제 {}", key);
   }
 }
