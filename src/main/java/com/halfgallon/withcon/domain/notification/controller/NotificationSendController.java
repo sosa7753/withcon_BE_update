7b85d3d9bd56b391ddb11fcf712638b5da2dd53b
sosa7753 <sosa7753@naver.com>
2025-01-19 22:46:14 +0900

    [013-KAFKA-GROUP-TREATMENT][feat] Group List를 파라미터로 받아 처리하는 URI 추가

--- src/main/java/com/halfgallon/withcon/domain/notification/controller/NotificationSendController.java
+++ src/main/java/com/halfgallon/withcon/domain/notification/controller/NotificationSendController.java
@@ -1,26 +1,34 @@
 package com.halfgallon.withcon.domain.notification.controller;
 
 import com.halfgallon.withcon.domain.notification.dto.NotificationResponse;
+import com.halfgallon.withcon.domain.notification.dto.NotificationResponses;
 import com.halfgallon.withcon.domain.notification.service.SseEmitterService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/notification")
+
 @RequiredArgsConstructor
 public class NotificationSendController {
 
   private final SseEmitterService sseEmitterService;
 
-  @PostMapping("/send")
+  @PostMapping("/notification/send")
   public ResponseEntity<Void> sendNotification(
       @RequestBody NotificationResponse notificationResponse) {
 
      sseEmitterService.sendNotificationToClient(notificationResponse);
      return ResponseEntity.ok().build();
+  }
+
+  @PostMapping("notifications/send")
+  public ResponseEntity<Void> sendNotifications(
+      @RequestBody NotificationResponses notificationResponses) {
+
+    sseEmitterService.sendMultiNotificationToClient(notificationResponses);
+    return ResponseEntity.ok().build();
   }
 }
